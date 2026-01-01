4a359c20f036574379a1a587ee54d7fd54b9adfb
donggi22 <muse1956@naver.com>
2026-01-01 18:02:28 +0900

    dropout 최신 구현 추가

--- 밑바닥부터 시작하는 딥러닝1/dropout.py
+++ 밑바닥부터 시작하는 딥러닝1/dropout.py
@@ -4,12 +4,21 @@ class Dropout:
         self.dropout_ratio = dropout_ratio
         self.mask = None
     
+    # 초기 구현 방식: 학습 때 평균 출력 ↓, 추론 때 기대값 맞추기 위해 스케일링
+    # def forward(self, x, train_flg=True):
+    #     if train_flg:
+    #         self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+    #         return x * self.mask
+    #     else:
+    #         return x * (1.0 - self.dropout_ratio) # 추론 시 각 뉴런의 출력에 훈련 때 삭제 안 한 비율을 곱하여 출력
+    
+    # 학습 시 스케일링 (요즘 표준): PyTorch / TensorFlow
     def forward(self, x, train_flg=True):
         if train_flg:
-            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
-            return x * self.mask
+            mask = np.random.rand(*x.shape) > self.dropout_ratio
+            y = x * mask / (1.0 - self.dropout_ratio)
         else:
-            return x * (1.0 - self.dropout_ratio)
+            return x
     
     def backward(self, dout):
         return dout * self.mask
