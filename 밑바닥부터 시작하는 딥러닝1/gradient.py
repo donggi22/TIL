11b498478399ac7b4a59a84152e10662f0c1f49f
donggi22 <muse1956@naver.com>
2026-01-01 14:52:12 +0900

    gradient.py 2차원 텐서도 가능하게 수정

--- 밑바닥부터 시작하는 딥러닝1/gradient.py
+++ 밑바닥부터 시작하는 딥러닝1/gradient.py
@@ -1,20 +1,60 @@
 import numpy as np
 
+# def numerical_gradient(f, x):
+#     h = 1e-4
+#     grad = np.zeros_like(x)
+    
+#     if x.ndim == 1:
+#         for idx in range(x.size):
+#             tmp_val = x[idx]
+
+#             # f(x+h)
+#             x[idx] = tmp_val + h
+#             fxh1 = f(x)
+
+#             # f(x-h)
+#             x[idx] = tmp_val - h
+#             fxh2 = f(x)
+
+#             grad = (fxh1 - fxh2) / (2*h)
+#         return grad
+
+
+#     elif x.ndim == 2:
+#         for idx0 in range(x.shape[0]):
+#             for idx1 in range(x.shape[1]):
+#                 tmp_val = x[idx0, idx1]
+            
+#                 # f(x+h)
+#                 x[idx0, idx1] = tmp_val + h
+#                 fxh1 = f(x)
+                
+#                 # f(x-h)
+#                 x[idx0, idx1] = tmp_val -h
+#                 fxh2 = f(x)
+                
+#                 grad[idx0, idx1] = (fxh1 - fxh2) / (2*h)
+#                 x[idx0, idx1] = tmp_val # 값 복원
+#         return grad
+
 def numerical_gradient(f, x):
     h = 1e-4
     grad = np.zeros_like(x)
-    
-    for idx in range(x.size):
+
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+
         tmp_val = x[idx]
-    
-        # f(x+h)
-        x[idx] = tmp_val + h
+        x[idx] = float(tmp_val) + h
         fxh1 = f(x)
-        
-        # f(x-h)
-        x[idx] = tmp_val -h
+
+        x[idx] = float(tmp_val) - h
         fxh2 = f(x)
-        
-        grad[idx] = (fxh1 - fxh2) / (2*h)
-        x[idx] = tmp_val # 값 복원
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val
+
+        it.iternext()
+
     return grad
--- 밑바닥부터 시작하는 딥러닝1/gradient_simplenet.py
+++ 밑바닥부터 시작하는 딥러닝1/gradient_simplenet.py
@@ -16,13 +16,15 @@ class SimpleNet:
         return loss
     
 net = SimpleNet()
-# print(net.W)
+# print(net.W) # shape: (2, 3), size: 6
 x = np.array([0.6, 0.9])
+# print(x.size) # 2
 pred = net.predict(x) # shape: (3,)
 # print(pred)
 # print(np.argmax(pred).shape) # shape: ()
 t = np.array([0, 0, 1]) # 정답 레이블
-print(net.loss(x, t))
+# print(t.shape) # shape: (3,)
+# print(net.loss(x, t)) # shape: ()
 
 def f(W):
     return net.loss(x, t)
