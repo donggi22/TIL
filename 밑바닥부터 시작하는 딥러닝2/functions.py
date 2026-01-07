29cacccc6ec4d365c853e9e688b29b54a405a446
donggi22 <muse1956@naver.com>
2026-01-07 12:56:20 +0900

    binary_cross_entropy 추가

--- 밑바닥부터 시작하는 딥러닝2/functions.py
+++ 밑바닥부터 시작하는 딥러닝2/functions.py
@@ -17,6 +17,16 @@ def softmax(x):
         x = np.exp(x) / np.exp(x).sum()
     return x
 
+def binary_cross_entropy(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    y = np.clip(y, 1e-7, 1 - 1e-7)
+
+    batch_size = y.shape[0]
+    return -np.sum(t*np.log(y + 1e-7) + (1-t)*np.log(1 - y + 1e-7)) / batch_size
+
 def cross_entropy_error(y, t):
     if y.ndim == 1:
         t = t.reshape(1, t.size)
