48196c9006fcd8db51e6036500eb0f98fb4bdea1
donggi22 <muse1956@naver.com>
2026-02-27 22:48:50 +0900

    함수를 더 편리하게

---
+++ 밑바닥부터 시작하는 딥러닝3/steps/step09.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+    
+    def backward(self):
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop() # 함수를 가져온다.
+            x, y = f.input, f.output # 함수의 입력과 출력을 가져온다.
+            x.grad = f.backward(y.grad) # backward 메서드를 호출한다.
+
+            if x.creator is not None:
+                funcs.append(x.creator) # 하나 앞의 함수를 리스트에 추가한다.
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        output.set_creator(self) # 출력 변수에 창조를 설정한다. 여기서 self는 메서드를 호출하는 객체(함수 클래스, func에 대응되는 객체)
+        self.input = input
+        self.output = output # 출력도 저장한다.
+        return output
+    
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+
+class Exp(Function):
+    def forward(self, x):
+        y = np.exp(x)
+        return y
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+def square(x):
+    return Square()(x) # 한 줄로 작성
+
+def exp(x):
+    return Exp()(x)
+
+x = Variable(np.array(0.5))
+y = square(exp(square(x))) # 연속하여 적용
+y.grad = np.array(1.0)
+y.backward()
+print(x.grad)
