7962ed25131774c3a8ab464c64f84e117bd4f8e5
donggi22 <muse1956@naver.com>
2026-03-09 20:58:17 +0900

    반복문 계산그래프

--- 밑바닥부터 시작하는 딥러닝3/scratch.py
+++ 밑바닥부터 시작하는 딥러닝3/scratch.py
@@ -1,5 +1,18 @@
 import numpy as np
 from dezero.core_simple import Variable
+from dezero.utils import plot_dot_graph
 
-x = Variable(np.array(1.0))
-print(x)
+x = Variable(np.array(3.0))
+y = Variable(np.array(0.0))
+
+while True:
+    y = y + x
+    if y.data > 100:
+        break
+
+y.backward()
+print(x.grad)
+
+x.name = 'x'
+y.name = 'y'
+plot_dot_graph(y, verbose=False, to_file='반복문.png')
