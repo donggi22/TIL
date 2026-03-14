1340912d3d1d5f180fb334838451ecc501927dba
donggi22 <muse1956@naver.com>
2026-03-14 23:43:37 +0900

    고차 미분

---
+++ 밑바닥부터 시작하는 딥러닝3/dezero/core.py
@@ -0,0 +1,240 @@
+import weakref
+import numpy as np
+import contextlib
+
+class Config:
+    enable_backprop = True
+
+# contextlib.contextmanager 데코레이터를 달면
+# with 진입 시 next(generator)
+# with 종료 시 정상: next(generator), 예외: generator.throw(...)
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value) # Config의 name 속성을 새로운 값(value)로 설정
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value) # Config의 name 속성을 원래 값(old_value)으로 복원
+
+def no_grad(): # using_config('enable_backprop', False) 간편화
+    return using_config('enable_backprop', False)
+
+class Variable:
+    __array_priority__ = 200 # NumPy 배열과 연산 시 이 객체의 연산이 우선되도록 설정
+
+    def __init__(self, data, name=None):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}은(는) 지원하지 않습니다.'.format(type(data)))
+        self.data = data
+        self.name = name
+        self.grad = None
+        self.creator = None
+        self.generation = 0 # 세대 수를 기록하는 변수
+
+    @property
+    def shape(self):
+        return self.data.shape
+    
+    @property
+    def ndim(self):
+        return self.data.ndim
+    
+    @property
+    def size(self):
+        return self.data.size
+    
+    @property
+    def dtype(self):
+        return self.data.dtype
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n' + ' ' * 9)
+        return 'variable(' + p + ')'
+
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1 # 세대를 기록한다(부모 세대 + 1).
+    
+    def cleargrad(self): # 토치의 optimizer.zerograd()와 유사한 듯
+        self.grad = None
+
+    def backward(self, retain_grad=False, create_graph=False): # create_graph로 gradient 계산과정을 계산그래프로 남길지 결정
+        if self.grad is None:
+            # self.grad = np.ones_like(self.data)
+            self.grad = Variable(np.ones_like(self.data)) # 미분값(ndarray) 저장에서 Variable 인스턴스를 저장하도록 변경
+
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set: # 계산 그래프가 분기될 때 동일 함수 재방문 방지
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation) # 큰 세대 순으로 pop하기 위해 정렬
+        
+        add_func(self.creator)
+
+        while funcs:
+            f = funcs.pop() # 함수를 가져온다.
+            # 수정 전: gys = [output.grad for output in f.outputs]
+            gys = [output().grad for output in f.outputs]
+
+            with using_config('enable_backprop', create_graph):
+                # backward 내부 연산에서 Function.__call__()이 실행될 때 Config.enable_backprop 값을 참조하여 역전파 그래프 생성(create_graph) 여부 결정
+                gxs = f.backward(*gys) # 메인 backward
+                if not isinstance(gxs, tuple):
+                    gxs = (gxs,)
+
+                for x, gx in zip(f.inputs, gxs):
+                    if x.grad is None:
+                        x.grad = gx
+                    else: # 누적 추가
+                        x.grad = x.grad + gx # 이 계산도 계산 그래프 생성 여부 결정
+
+                    if x.creator is not None:
+                        add_func(x.creator) # 수정 전: funcs.append(x.creator)
+
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None # y는 약한 참조(weakref)라서 ()로 호출
+
+def as_variable(obj):
+    if isinstance(obj, Variable):
+        return obj
+    return Variable(obj)
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+class Function:
+    def __call__(self, *inputs): # 입출력이 다변수일 때
+        inputs = [as_variable(x) for x in inputs]
+
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs) # unpack
+        if not isinstance(ys, tuple): # 튜플이 아닌 경우 튜플로
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys] 
+        
+        if Config.enable_backprop: # 역전파 활성
+            self.generation = max([x.generation for x in inputs])
+            for output in outputs:
+                output.set_creator(self)
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
+
+        return outputs if len(outputs) > 1 else outputs[0]
+        
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
+        raise NotImplementedError()
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+    
+    def backward(self, gy):
+        return gy, gy
+
+def add(x0, x1):
+    x1 = as_array(x1)
+    return Add()(x0, x1)
+
+class Mul(Function):
+    def forward(self, x0, x1):
+        y = x0 * x1
+        return y
+    
+    def backward(self, gy):
+        x0, x1 = self.inputs
+        return gy * x1, gy * x0
+
+def mul(x0, x1):
+    x1 = as_array(x1)
+    return Mul()(x0, x1)
+
+class Neg(Function):
+    def forward(self, x):
+        return -x
+    
+    def backward(self, gy):
+        return -gy
+
+def neg(x):
+    return Neg()(x)
+
+class Sub(Function):
+    def forward(self, x0, x1):
+        y = x0 - x1
+        return y
+    
+    def backward(self, gy):
+        return gy, -gy
+    
+def sub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x0, x1)
+
+def rsub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x1, x0) # x0와 x1의 순서를 바꾼다.
+
+class Div(Function):
+    def forward(self, x0, x1):
+        y = x0 / x1
+        return y
+    
+    def backward(self, gy):
+        x0, x1 = self.inputs
+        gx0 = gy / x1
+        gx1 = gy * (-x0 / x1 ** 2)
+        return gx0, gx1
+    
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x1, x0) # x0와 x1의 순서를 바꾼다.
+
+class Pow(Function):
+    def __init__(self, c):
+        self.c = c
+    
+    def forward(self, x):
+        y = x ** self.c
+        return y
+    
+    def backward(self, gy):
+        x, = self.inputs # self.inputs = [as_variable(x) for x in inputs]
+        c = self.c
+        gx = c * x ** (c - 1) * gy
+        return gx
+    
+def pow(x, c):
+    return Pow(c)(x)
+
+def setup_variable():
+    Variable.__add__ = add
+    Variable.__radd__ = add
+    Variable.__mul__ = mul
+    Variable.__rmul__ = mul
+    Variable.__neg__ = neg
+    Variable.__sub__ = sub
+    Variable.__rsub__ = rsub
+    Variable.__truediv__ = div
+    Variable.__rtruediv__ = rdiv
+    Variable.__pow__ = pow
